808bf4bb7b955037471b1f4e827f2ffd2183aebb
kenzosashida090 <kenzosashida1@gmail.com>
2026-04-30 09:57:24 -0600

    [new] adding section products table

--- dash-admin-front/src/scenes/products/index.tsx
+++ dash-admin-front/src/scenes/products/index.tsx
@@ -2,27 +2,27 @@ import Header from '@/components/Header'
 import ProductCard from '@/components/ProductCard'
 import { useGetProductsQuery } from '@/state/api'
 import { Box, Typography, useMediaQuery, useTheme } from '@mui/material'
-import React from 'react'
+
 
 const Products = () => {
-  const {data:products, isLoading, isError} =useGetProductsQuery()
+  const { data: products, isLoading, isError } = useGetProductsQuery()
   const isNonMobile = useMediaQuery("(min-width:1000px)")
   const theme = useTheme()
-  if(isError) {
-    return       <Typography
-            variant='h2'
-            color={theme.palette.secondary[100]}
-            fontWeight="bold"
-            sx={{mb:"5px"}}
-            >
-                {"Error"}
-            </Typography>
+  if (isError) {
+    return <Typography
+      variant='h2'
+      color={theme.palette.secondary[100]}
+      fontWeight="bold"
+      sx={{ mb: "5px" }}
+    >
+      {"Error"}
+    </Typography>
   }
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title='Products'  subtitle='See your list of products' />
+      <Header title='Products' subtitle='See your list of products' />
       {
-        products || !isLoading  ?(
+        products || !isLoading ? (
           <Box
             mt="20px"
             display={'grid'}
@@ -31,27 +31,27 @@ const Products = () => {
             rowGap={"24px"}
             columnGap={"1.33%"}
             sx={{
-              "& > div": { gridColumn: isNonMobile ? undefined : "span 4"} // if its mobile us the entire 4 span column
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" } // if its mobile us the entire 4 span column
             }}
           >
             {
-              products?.map((product, id)=>(
+              products?.map((product, id) => (
                 <ProductCard {...product} key={id} />
               ))
             }
 
           </Box>
-        ):
-        (
-      <Typography
-        variant='h2'
-        color={theme.palette.secondary[100]}
-        fontWeight="bold"
-        sx={{mb:"5px"}}
-        >
-           {"Loading"}
-        </Typography>
-        )
+        ) :
+          (
+            <Typography
+              variant='h2'
+              color={theme.palette.secondary[100]}
+              fontWeight="bold"
+              sx={{ mb: "5px" }}
+            >
+              {"Loading"}
+            </Typography>
+          )
       }
     </Box>
   )

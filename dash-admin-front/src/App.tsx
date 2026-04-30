113f69a8b17f66d49b435f4003e2372ff37e56fe
kenzosashida090 <kenzosashida1@gmail.com>
2026-04-30 09:54:12 -0600

    [new] Adding routes

--- dash-admin-front/src/App.tsx
+++ dash-admin-front/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { CssBaseline, ThemeProvider} from "@mui/material"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { themeSettings } from '@/theme'
 import { useAppSelector } from './state/redux'
@@ -12,23 +12,31 @@ import Login from '@/scenes/auth/Login'
 import Products from '@/scenes/products'
 import Customers from '@/scenes/customers'
 import Transactions from './scenes/transaction'
+import Signup from './scenes/auth/Signup'
+import Geography from './scenes/geo'
+import OverallStats from './scenes/overview'
+import DailyStats from './scenes/daily'
 function App() {
-  const mode = useAppSelector((state)=>state.global.mode)
-  const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
+  const mode = useAppSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className='app'>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route element={<Layout/>}>
-              <Route path='/' element={<Navigate to="/dashboard" replace/>} />
-              <Route path='/dashboard' element={<Dashboard/>} />
-              <Route path='/products' element={<Products/>} />
-              <Route path='/customers' element={<Customers/>} />
-              <Route path='/transaction' element={<Transactions/>} />
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to="/dashboard" replace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/products' element={<Products />} />
+              <Route path='/customers' element={<Customers />} />
+              <Route path='/transaction' element={<Transactions />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/overview' element={<OverallStats />} />
+              <Route path='/daily' element={<DailyStats />} />
             </Route>
-            <Route path='/login' element={<Login/>} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>

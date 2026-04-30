7d5b6877a9ce2f9f79e054af0a8d883ae29d5ded
kenzosashida090 <kenzosashida1@gmail.com>
2026-04-30 09:52:28 -0600

    [new] adding scene for customers section

--- dash-admin-front/src/scenes/customers/index.tsx
+++ dash-admin-front/src/scenes/customers/index.tsx
@@ -1,101 +1,103 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import Header from '@/components/Header'
 import { useGetCustomersQuery } from '@/state/api'
 import { useAppSelector } from '@/state/redux';
 import { Box, useTheme } from '@mui/material'
-import { DataGrid,  } from '@mui/x-data-grid';
-import { useSelector } from 'react-redux';
+import { DataGrid } from '@mui/x-data-grid';
+import type { GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
 
-type CellParams = {
-    value: string
-}
+
+
 const Customers = () => {
-    const {data:customers, isLoading} = useGetCustomersQuery()
-    const theme = useTheme()
-    const colorTheme = useAppSelector((state)=>state.global.mode)
-    const customersColumns= [
-        {
-            field: "id",
-            headerName: "ID",
-            flex:1,
-        },
-        {
-            field: "name",
-            headerName: "Name",
-            flex:0.5,
-        },
-        {
-            field: "email",
-            headerName: "Email",
-            flex:1,
-        },
-        {
-            field: "phoneNumber",
-            headerName: "Phone Number",
-            flex:0.5,
-            rednerCell:(params:CellParams)=> {
-                return params.value.replace(/^(\d{3})(\d{3})(\d{4})/,"($1)$2-$3")
-            }
-        },
-        {
-            field: "country",
-            headerName: "Country",
-            flex:0.4,
+  const { data: customers, isLoading } = useGetCustomersQuery()
+  const theme = useTheme()
 
-        },
-        {
-            field: "occupation",
-            headerName: "Occupation",
-            flex:1,
 
-        },
-        {
-            field: "role",
-            headerName: "Role",
-            flex:0.5,
+  const colorTheme = useAppSelector((state) => state.global.mode)
+  if (!customers) { return <p>HEllo</p> }
+  const customersColumns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params: GridRenderCellParams) => {
+        return <span>{params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3")}</span>
+      }
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
 
-        },
-    ]
-    return (
-        <Box  m="1.5rem 2.5rem">
-            <Header title='Customers' subtitle='I dont know what to do here'/>
-            <Box
-                mt={"40px"}
-                height={"75vh"}
-                sx={{
-                    "& .MuiDataGrid-columnHeadersInner": {
-                        border: "none"
-                    },
-                    "& .MuiDataGrid-cell":{
-                        borderBottom:"none"
-                    },
-                    "& .MuiDataGrid-columnHeaders":{
-                        backgroundColor: theme.palette.background.paper,
-                        color:theme.palette.secondary[100],
-                        borderBottom:"none"
-                    },
-                    "& .MuiDataGrid-virtualScroller":{
-                        backgroundColor: colorTheme === "dark" ? theme.palette.secondary[600] : theme.palette.background.paper ,
-                    },
-                    "& .MuiDataGrid-footerContainer":{
-                        backgroundColor: theme.palette.background.paper,
-                        color:theme.palette.secondary[100],
-                        borderBottom:"none"
-                    },
-                    "& .MuiDataGrid-toolbarContainer .MuiButton-text":{
-                        color:`${theme.palette.secondary[200]} !important`,
-                    },
-                }}
-            >
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
 
-            <DataGrid
-                loading={isLoading || !customers}
-                rows={ customers || []}
-                getRowId={(row)=> row.id}
-                columns={customersColumns}
-                />
-            </Box>
-        </Box>
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+
+    },
+  ]
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title='Customers' subtitle='I dont know what to do here' />
+      <Box
+        mt={"40px"}
+        height={"75vh"}
+        sx={{
+          "& .MuiDataGrid-columnHeadersInner": {
+            border: "none"
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.secondary[100],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colorTheme === "dark" ? theme.palette.secondary[600] : theme.palette.background.paper,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.secondary[100],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+
+        <DataGrid
+          loading={isLoading || !customers}
+          rows={customers || []}
+          getRowId={(row) => row.id}
+          columns={customersColumns}
+        />
+      </Box>
+    </Box>
   )
 }
 

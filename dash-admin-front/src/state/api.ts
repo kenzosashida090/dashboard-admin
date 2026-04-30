0ae84cd8b57fd650900bcd42d454e53c3d5644ac
kenzosashida090 <kenzosashida1@gmail.com>
2026-04-30 09:56:14 -0600

    [new] setting the services fcrom api customers products and charts

--- dash-admin-front/src/state/api.ts
+++ dash-admin-front/src/state/api.ts
@@ -1,40 +1,76 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
+type TransactionsResponse = {
+  transactions: Transactions[]
+  total: number
+
+}
 
 export const api = createApi({
-    baseQuery:fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL,
-        credentials:'include'
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    credentials: 'include'
+  }),
+  reducerPath: "api",
+  tagTypes: ["User", "Products", "Customers", "Transactions"],
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      query: () => ({
+        url: `general/user`
+      }),
+      providesTags: ["User"]
     }),
-    reducerPath:"api",
-    tagTypes: ["User", "Products","Customers"],
-    endpoints: (build)=>({
-        getAuthUser: build.query<User, void>({
-            query:()=>({
-                url:`general/user`
-            }),
-            providesTags:["User"]
-        }),
-        login: build.mutation<User, UserSignIn>({
-            query:({...userData})=>({
-                url:'auth/signin',
-                method:'POST',
-                body: userData
-            })
-        }),
-        getProducts: build.query<Products[], void>({
-            query:()=>({
-                url:'client/products'
-            }),
-            providesTags:["Products"]
-        }),
-        getCustomers: build.query<User[],void>({
-            query:()=>({
-                url:'client/customers'
-            }),
-            providesTags:["Customers"]
+    login: build.mutation<User, UserSignIn>({
+      query: ({ ...userData }) => ({
+        url: 'auth/signin',
+        method: 'POST',
+        body: userData
+      })
+    }),
+    signup: build.mutation<User, UserSignIn>({
+      query: ({ ...userData }) => ({
+        url: 'auth/signup',
+        method: 'POST',
+        body: userData
+      })
+    }),
+    getProducts: build.query<Products[], void>({
+      query: () => ({
+        url: 'client/products'
+      }),
+      providesTags: ["Products"]
+    }),
+    getCustomers: build.query<User[], void>({
+      query: () => ({
+        url: 'client/customers'
+      }),
+      providesTags: ["Customers"]
+    }),
+    getTransactions: build.query<TransactionsResponse, TransactionFilter>({
+      query: ({ page, pageSize, search, sort }) => {
+        return ({
+          url: 'client/transactions',
+          params: { page, pageSize, search, sort: JSON.stringify(sort) },
+
         })
+      },
+      providesTags: ["Transactions"]
+    }),
+    getGeography: build.query<GeographyResponse[], void>({
+      query: () => {
+        return ({
+          url: 'client/geography',
+        })
+      }
+    }),
+    getOverallStats: build.query<OverallStatsResponse, void>({
+      query: () => {
+        return ({
+          url: '/sales/sales'
+        })
+      }
     })
+  })
 })
 
-export const {  useLoginMutation, useGetAuthUserQuery, useGetProductsQuery, useGetCustomersQuery } = api
+export const { useLoginMutation, useSignupMutation, useGetAuthUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetOverallStatsQuery, useGetTransactionsQuery, useGetGeographyQuery } = api
